5bde0c01c53737928056a95d67c76308c89201ca
Restyled.io <commits@restyled.io>
2020-09-06 13:51:07 +0000

    Restyled by prettier

--- client/app/pages/dashboards/hooks/useAddDashboardToRecentDashboards.js
+++ client/app/pages/dashboards/hooks/useAddDashboardToRecentDashboards.js
@@ -8,12 +8,17 @@ export default function useAddDashboardToRecentDashboards(dashboardId) {
       localStorage.setItem("recentDashboards", JSON.stringify([{ id: dashboardId, priority: 1 }]));
     } else {
       const parsedCurrentRecentDashboards = JSON.parse(currentRecentDashboards);
-      const currentRecentDashboardsPrioritys = map(parsedCurrentRecentDashboards, recentDashboard => recentDashboard.priority);
+      const currentRecentDashboardsPrioritys = map(
+        parsedCurrentRecentDashboards,
+        recentDashboard => recentDashboard.priority
+      );
       const recentDashboardsIncludesCurrentDashboard =
         find(parsedCurrentRecentDashboards, recentDashboard => recentDashboard.id === dashboardId) !== undefined;
       const biggestPriorityValue = reduce(currentRecentDashboardsPrioritys, (a, b) => Math.max(a, b));
       if (recentDashboardsIncludesCurrentDashboard) {
-        const newRecentDashboards = map(parsedCurrentRecentDashboards, recentDashboard => recentDashboard.id === dashboardId ? { id: dashboardId, priority: biggestPriorityValue + 1 } : recentDashboard);
+        const newRecentDashboards = map(parsedCurrentRecentDashboards, recentDashboard =>
+          recentDashboard.id === dashboardId ? { id: dashboardId, priority: biggestPriorityValue + 1 } : recentDashboard
+        );
         localStorage.setItem("recentDashboards", JSON.stringify(newRecentDashboards));
       } else {
         parsedCurrentRecentDashboards.push({ id: dashboardId, priority: biggestPriorityValue + 1 });
